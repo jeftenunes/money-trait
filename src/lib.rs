a8256ed48ece968daf4a4a0a394a3cef803f8373
Santos Nunes <sanje04@mes.reseau.intra>
2019-11-18 15:53:15 -0500

    [master] - packt course money trait

---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+
+#[derive(PartialEq, Debug)]
+pub struct BRL(i32);
+#[derive(PartialEq, Debug)]
+pub struct USD(i32);
+#[derive(PartialEq, Debug)]
+pub struct GBP(i32);
+#[derive(PartialEq, Debug)]
+pub struct CAD(i32);
+
+pub trait ToUSD {
+    fn to_usd(&self) -> USD;
+}
+
+impl ToUSD for GBP {
+    fn to_usd(&self) -> USD {
+        USD((self.0 * 130) / 100)
+    }
+}
+
+pub trait FromUSD {
+    fn from_usd(u:&USD) -> Self;
+}
+
+impl FromUSD for CAD {
+    fn from_usd(u: &USD) -> Self {
+        CAD((u.0 * 130) / 100)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let gbp = GBP(200);
+        let usd = gbp.to_usd();
+
+        let cad = CAD::from_usd(&USD(10));
+
+        assert_eq!(usd, USD(260));
+        assert_eq!(cad, CAD(13));
+    }
+}
